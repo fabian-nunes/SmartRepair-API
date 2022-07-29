55358a2c5bd49e07982887534a32aac5e7676fa7
fabian-nunes <fabiannunes12@gmail.com>
2022-07-29 12:21:19 +0100

    Added base express files

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+//Import Routes
+const repairsRoute = require('./routes/repairs');
+
+//Middlewares
+app.use('/repairs', repairsRoute);
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true } ,() => {
+   console.log("Connected to DB");
+});
+
+//Listen
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
